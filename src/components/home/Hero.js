32b822085c17e36f2487315e4f9af9c62b8045f8
Maneesh Kumar Pal <86200227+Austin73@users.noreply.github.com>
2026-09-18 09:46:14 +0530

    Update Hero.js

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -71,7 +71,7 @@ export default function Hero() {
       <div
         className="absolute inset-0 -z-10 h-full w-full bg-cover bg-center"
         style={{
-          backgroundImage: "url(/images/hero/background.png)",
+          backgroundImage: "url(/images/hero/background.webp)",
         }}
       />
 
